0b9d43f21b606256fd744962a15f735c311aae74
Eric Melz <emelz@gumgum.com>
2023-07-14 07:47:21 -0700

    add Button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,15 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+    const onButtonClick = () => { console.log("Button is pushed")};
     return (
         <div>
             <Alert>
                 <b>Hello World</b>
             </Alert>
+            <Button onClick={onButtonClick}>Push Me</Button>
         </div>
     )
 }
