b27820fcaa958ef279d4351e915daa40f6080a77
ericmelz <eric@emelz.com>
2023-07-29 06:50:50 -0700

    use styled-components

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,20 @@
 import {useState} from "react";
-import styles from "./ListGroup.module.css"
+import "./ListGroup.css"
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`
+
+interface ListItemProps {
+    active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${ props => props.active ? 'blue' : 'none'}
+`
 
 interface Props {
     items: string[];
@@ -15,9 +30,10 @@ function ListGroup({items, heading, onSelectItem}: Props) {
         <>
             <h1>{heading}</h1>
             {items.length === 0 && <p>No item found</p>}
-            <ul className={[styles.listGroup, styles.container].join(' ')}>
+            <List>
                 {items.map((item, index) =>
-                    <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                    <ListItem
+                        active={index === selectedIndex}
                         key={item}
                         onClick={
                             () => {
@@ -26,9 +42,9 @@ function ListGroup({items, heading, onSelectItem}: Props) {
                             }}
                     >
                         {item}
-                    </li>
+                    </ListItem>
                 )}
-            </ul>
+            </List>
         </>
     );
 
